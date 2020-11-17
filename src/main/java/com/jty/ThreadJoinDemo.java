f31c5c19de4953f5c53c97f3696e3b004388d631
jty <420671626@qq.com>
2020-11-17 22:51:20 +0800

    change pom

---
+++ src/main/java/com/jty/ThreadJoinDemo.java
@@ -0,0 +1,10 @@
+package com.jty;
+
+public class ThreadJoinDemo {
+    public static void main(String[] args) {
+
+        boolean interrupted = Thread.interrupted();
+
+        System.out.println("这是一个测试");
+    }
+}
